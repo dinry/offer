a4c5171b13f59d14483c3f016174c6dd202dfd50
dinry <1138316631@qq.com>
2019-04-08 14:08:47 +0800

    Update 二维数组中的查找.java

--- 二维数组中的查找.java
+++ 二维数组中的查找.java
@@ -1,1 +1,23 @@
+package sword2offer;
+
+public class offer1_solution {
+	public boolean Find(int target, int [][] array) {
+		if(array.length==0||array[0].length==0)
+			return false;
+		int rows=array.length-1;
+		int cols=array[0].length-1;
+		int i=0;
+		int j=cols;
+		while(i<=rows && j>=0) {
+			if(array[i][j]<target) {
+				i++;
+			}else if(array[i][j]>target){
+				j--;
+			}else {
+				return true;
+			}
+		}
+		return false;
+	}
+}
 
